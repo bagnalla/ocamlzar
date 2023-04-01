ea014b401eccc570351c021a9d66d27585022750
Alex <ab667712@ohio.edu>
2023-04-01 15:46:50 -0400

    Fix comment

--- lib/die.mli
+++ lib/die.mli
@@ -1,6 +1,6 @@
 (** This module exposes a basic interface for building and drawing
     samples from an n-sided die (uniform distribution over the range
-    [0,n) of integers). *)
+    \[0,n) of integers). *)
 
 (** Build and cache die. *)
 val build : int -> unit
